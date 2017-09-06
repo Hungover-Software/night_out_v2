e4fda7d4a0d9be92448a16d6cd0647afdaede8e2
Logan Fitzgibbons <logiwan992@gmail.com>
2017-09-06 17:05:11 +0000

    Initial routing structure for issue #10

---
+++ imports/ui/layouts/layout.js
@@ -0,0 +1,9 @@
+import './layout.html';
+import '../navbar.js';
+
+import '../public/login.js';
+import '../public/signup.js';
+
+import '../private/landing.js';
+import '../private/friends.js';
+import '../private/event.js';
