87affe0826c04bec98b4d6aa90884d8f3269f011
Cameron Mezzell <cmezzell@unomaha.edu>
2017-09-22 20:18:10 +0000

    Issue #15 You can seed data now. Edit server/seed.js

---
+++ server/seed.js
@@ -0,0 +1,31 @@
+if (Meteor.isServer && Meteor.isDevelopment) {
+  Meteor.startup(function() {
+    if (Meteor.users.find().count() === 0) {
+      var accounts = [
+        {
+          email: "test@test.com",
+          username: "test",
+          password: "password",
+        },
+        {
+          email: "test2@test.com",
+          username: "test2",
+          password: "password",
+        },
+        {
+          email: "test3@test.com",
+          username: "test3",
+          password: "password",
+        },
+      ];
+
+      _.each(accounts, function(account) {
+        Accounts.createUser({
+          email: account.email,
+          username: account.username,
+          password: account.password,
+        });
+      });
+    }
+  })
+}
