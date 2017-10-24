f260a0efab3ebb33f343c8c1600014aff1f99d73
Cameron Mezzell <cmezzell@unomaha.edu>
2017-10-24 18:35:13 +0000

    Committing to grab master branch changes.

--- imports/api/groups.js
+++ imports/api/groups.js
@@ -1,6 +1,56 @@
 import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+import { BasicUserInfoSchema, GetBasicUserInfo } from './basic-user-info';
 
-export const Group = new Mongo.Collection('groups');
+export const Groups = new Mongo.Collection('groups');
+
+if (Meteor.isServer) {
+    Meteor.publish('groups', function tasksPublication() {
+        return Groups.find({userId: this.userId});
+    });
+
+    Meteor.methods({
+        'groups.newGroup'(name) {
+            // User must be logged in
+            if (! this.userId) {
+                throw new Meteor.Error('not-authorized');
+            }
+
+            // Ensure the given group name is not empty
+            if (name.length == 0) {
+                throw new Meteor.Error('Group name cannot be empty');
+            }
+
+            // Ensure they don't have a group with the same name
+            if (Groups.find({userId: this.userId, groupName: name}).count() > 0) {
+                throw new Meteor.Error('User already has a group with the given name');
+            }
+
+            // Create the group
+            Groups.insert({
+                userId: this.userId,
+                groupName: name,
+                friends: [],
+            });
+
+            return {success: true};
+        },
+
+        'groups.addGroupMembers'(groupId, friendsList) {
+            // User must be logged in
+            if (! this.userId) {
+                throw new Meteor.Error('not-authorized');
+            }
+
+            // Ensure they are adding at least one person
+            if (friendsList.length == 0) {
+                throw new Meteor.Error('Must have at least one friend');
+            }
+
+            // Add the friends to the group
+            Groups.update({_id: groupId}, {$push: {'friends': friendsList}});
+        },
+    });
+}
 
 var groupSchema = new SimpleSchema({
     userId: {
@@ -11,12 +61,10 @@ var groupSchema = new SimpleSchema({
         type: String,
         label: 'Group Name',
     },
-    friends: [{
-        userId: {
-            type: String,
-            label: 'User ID',
-        },
-    }],
+    friends: {
+        type: [BasicUserInfoSchema],
+        label: 'Friends',
+    }
 });
 
-Group.attachSchema(groupSchema);
+Groups.attachSchema(groupSchema);
