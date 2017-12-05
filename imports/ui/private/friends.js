95eebda06d1f275b1d0fee37eae8606fd7063329
Cameron Mezzell <cmezzell@unomaha.edu>
2017-12-05 19:50:17 +0000

    I need to update this branch. Committing changes first.

--- imports/ui/private/friends.js
+++ imports/ui/private/friends.js
@@ -5,16 +5,33 @@ import './friends.html';
 import {Friends, FriendRequests} from '../../api/friends.js';
 import {Groups} from '../../api/groups.js';
 
-Template.friends.onCreated(function() {
+Template.friends_section.onCreated(function() {
     Meteor.subscribe('friends');
-    Meteor.subscribe('friendRequests');
 });
 
-Template.friends.helpers({
+Template.friends.onRendered(function() {
+    $('ul.tabs').tabs();
+});
+
+Template.friends_section.helpers({
   friendsList() {
     return Friends.find();
   },
+});
 
+Template.friends_section.events({
+  'click #unfriend'(event) {
+    event.preventDefault();
+
+    Meteor.call('friends.unfriend', this.friend.userId);
+  }
+})
+
+Template.friend_requests_section.onCreated(function() {
+  Meteor.subscribe('friendRequests');
+});
+
+Template.friend_requests_section.helpers({
   sentFriendRequestsList() {
     return FriendRequests.find({'sender.userId': Meteor.userId()});
   },
@@ -24,7 +41,7 @@ Template.friends.helpers({
   },
 });
 
-Template.friends.events({
+Template.friend_requests_section.events({
   'submit #request'(event) {
     event.preventDefault();
 
@@ -34,35 +51,28 @@ Template.friends.events({
     Meteor.call('friendRequests.insert', email);
   },
 
-  'submit #decline'(event) {
+  'click #decline'(event) {
     event.preventDefault();
 
     Meteor.call('friendRequests.decline', this._id);
   },
 
-  'submit #accept'(event) {
+  'click #accept'(event) {
     event.preventDefault();
 
     Meteor.call('friendRequests.accept', this._id);
   },
-
-  'submit #unfriend'(event) {
-    event.preventDefault();
-
-    Meteor.call('friends.unfriend', this.friend.userId);
-  },
 });
 
-Template.groups.onCreated(function () {
+Template.groups_section.onCreated(function() {
   Meteor.subscribe('groups');
-  Meteor.subscribe('friends');
 });
 
-Template.group.onRendered(function() {
-    $('.modal').modal();
+Template.groups_section.onRendered(function() {
+    $('ul.collapsible').collapsible();
 });
 
-Template.groups.helpers({
+Template.groups_section.helpers({
   groupsList() {
     return Groups.find();
   },
@@ -71,7 +81,7 @@ Template.groups.helpers({
   }
 });
 
-Template.groups.events({
+Template.groups_section.events({
   'submit #new_group'(event) {
     event.preventDefault();
 
@@ -79,72 +89,5 @@ Template.groups.events({
     const groupName = target.group_name.value;
 
     Meteor.call('groups.newGroup', groupName);
-  },
-
-  'click #group_modal'(event) {
-    event.preventDefault();
-    Session.set('currentGroup', Groups.findOne({groupName: this.groupName}).friends);
-  },
-
-  'submit #add_group_members'(event) {
-    event.preventDefault();
-
-    Meteor.call('groups.updateGroupMembers', this.groupName, Session.get('currentGroup'));
-
-    $('.modal.open').modal('close')
-  },
-
-  'submit #remove_group_member'(event) {
-    event.preventDefault();
-
-  },
-
-  'change #friends input'(event) {
-        const target = event.target;
-
-        let groupMembers = Session.get('currentGroup');
-
-        if (target.checked) {
-            let index = -1;
-
-            for (let i = 0; i < groupMembers.length; i++) {
-              if (groupMembers[i].userId == target.id) {
-                index = i;
-                break;
-              }
-            }
-
-            if (index === -1) {
-              groupMembers.push({userId: target.id, username: target.value, email: target.dataset.email});
-            }
-        }
-        else {
-            let index = 0;
-
-            for (let i = 0; i < groupMembers.length; i++) {
-              if (groupMembers[i].userId == target.id) {
-                index = i;
-                break;
-              }
-            }
-
-            groupMembers.splice(index, 1);
-        }
-
-        Session.set('currentGroup', groupMembers);
-    },
-});
-
-Template.group.onCreated(function () {
-  Meteor.subscribe('friends');
-});
-
-Template.group.onRendered(function() {
-    $('.modal').modal();
-});
-
-Template.group.helpers({
-  friendsList() {
-    return Friends.find();
   },
 });
