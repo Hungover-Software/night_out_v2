ab7b1899876ca51c5a9a13392e81e60a29056f2e
Logan Fitzgibbons <logiwan992@gmail.com>
2017-12-07 16:49:46 +0000

    Adding invitees to already created group

--- imports/ui/private/event.js
+++ imports/ui/private/event.js
@@ -1,18 +1,32 @@
 import { Materialize } from 'meteor/materialize:materialize';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { Random } from 'meteor/random';
+import { Session } from 'meteor/session';
 
 import './event.html';
 
-import {Events} from '../../api/events.js';
+import { Friends } from '../../api/friends.js';
+import { Events } from '../../api/events.js';
 import { Toasts } from '../../components/toasts.js';
+
+function sortedFriends (friends) {
+  friends.sort(function(a, b){
+    let x = a.friend.username.toLowerCase();
+    let y = b.friend.username.toLowerCase();
+    if (x < y) {return -1;}
+    if (x > y) {return 1;}
+    return 0;
+  });
+
+  return friends;
+}
 
 Template.event.onCreated(function() {
     Meteor.subscribe('events');
 });
 
 Template.comment_modal.onRendered(function() {
-  $('.modal').modal();
+  $('.modal#comment-modal').modal();
 });
 
 Template.event.helpers({
@@ -97,4 +111,82 @@ Template.event.events({
       }
     });
   },
+  'click #edit-invitees'(event) {
+    Session.set('currentInvitees', []);
+    Session.set('currentInvitees', Events.findOne({_id: FlowRouter.getParam('_id')}).invitees);
+  },
+});
+
+Template.friends_modal.onCreated(function() {
+    Meteor.subscribe('friends');
+});
+
+Template.friends_modal.onRendered(function() {
+  $('.modal#friends-modal').modal();
+});
+
+Template.friends_modal.helpers({
+  friendList(eventInvitees) {
+    return Friends.find();
+  },
+  isChecked(userId, invitees) {
+    for (let friend of invitees) {
+      if (friend.userId === userId) {
+        return 'checked';
+      }
+    }
+    return '';
+  },
+  nequal(userId, creatorId) {
+    return userId !== creatorId;
+  }, 
+});
+
+Template.friends_modal.events({
+  'click #invite'(event) {
+    Meteor.call('event.updateInvitees', FlowRouter.getParam('_id'), Session.get('currentInvitees'), function(err) {
+      if (err) {
+        Materialize.Toast.removeAll();
+        Toasts.error(err.reason, 4000, 'error_outline');
+      }
+      Session.set('currentInvitees', undefined);
+    });
+
+    
+  },
+
+  'change #friends input'(event) {
+        const target = event.target;
+
+        let invitees = Session.get('currentInvitees');
+
+        if (target.checked) {
+            let index = -1;
+
+            for (let i = 0; i < invitees.length; i++) {
+              if (invitees[i].userId == target.id) {
+                index = i;
+                break;
+              }
+            }
+
+            if (index === -1) {
+              invitees.push({userId: target.id, username: target.value, email: target.dataset.email});
+            }
+        }
+        else {
+            let index = 0;
+
+            for (let i = 0; i < invitees.length; i++) {
+              if (invitees[i].userId == target.id) {
+                index = i;
+                break;
+              }
+            }
+
+            invitees.splice(index, 1);
+        }
+
+        Session.set('currentInvitees', invitees);
+    },
 });
